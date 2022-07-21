559a54e15f0d52ad736042f3720feca28583c65e
Santiago Rios Valero <sriosv@correo.udsitrital.edu.co>
2022-07-20 22:33:09 -0500

    initial commit

---
+++ android/app/src/main/kotlin/app/seminario/proyecto_final_seminario_restaurante/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.seminario.proyecto_final_seminario_restaurante
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/widgets/widgets.dart
@@ -0,0 +1,10 @@
+export 'package:proyecto_final_seminario_restaurante/app/widgets/email_input.dart';
+export 'package:proyecto_final_seminario_restaurante/app/widgets/normal_input.dart';
+export 'package:proyecto_final_seminario_restaurante/app/widgets/password_input.dart';
+export 'package:proyecto_final_seminario_restaurante/app/widgets/product_card.dart';
+export 'package:proyecto_final_seminario_restaurante/app/widgets/purple_button.dart';
+export 'package:proyecto_final_seminario_restaurante/app/widgets/purple_outlined_button.dart';
+export 'package:proyecto_final_seminario_restaurante/app/widgets/simple_alert.dart';
+export 'package:proyecto_final_seminario_restaurante/app/widgets/snackbars.dart';
+export 'package:proyecto_final_seminario_restaurante/app/widgets/yellow_button.dart';
+export 'package:proyecto_final_seminario_restaurante/app/widgets/yellow_outlined_button.dart';
